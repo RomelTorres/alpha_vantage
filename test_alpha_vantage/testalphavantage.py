efa5812afff5f3004129a92ee600f7cfad9cf346
RomelTorres <torres.romel@gmail.com>
2017-04-29 18:29:22 +0200

    test: Add test for get daily data

--- test_alpha_vantage/testalphavantage.py
+++ test_alpha_vantage/testalphavantage.py
@@ -9,7 +9,7 @@ class TestAlphaVantage(unittest.TestCase):
     """
     _API_KEY_TEST = '486U'
     _API_EQ_NAME_TEST = 'MSFT'
-    
+
     def test_key_none(self):
         """Raise an error when a key has not been given
         """
@@ -48,6 +48,39 @@ class TestAlphaVantage(unittest.TestCase):
         self.assertTrue(len(data) == 100, "The dictionary size for the default \
         outputsize ('compact') has to be 100")
         data, meta_data = av.get_intraday(TestAlphaVantage._API_EQ_NAME_TEST,
+        outputsize='full')
+        self.assertTrue(len(data) > 100, 'The dictionary size for the default \
+        outputsize has to be 100')
+
+    def test_get_daily_is_dict(self):
+        """Result must be a dictionary containning the json data
+        """
+        av = AlphaVantage(key=TestAlphaVantage._API_KEY_TEST)
+        data, meta_data = av.get_daily(TestAlphaVantage._API_EQ_NAME_TEST)
+        self.assertIsInstance(data, dict, 'Result Data must be a dictionary')
+        self.assertIsInstance(meta_data, dict, 'Result Meta Data must be a \
+        dictionary')
+
+    def test_daily_error(self):
+        """A Value error must be raised when a parameter is wrong
+        """
+        av = AlphaVantage(key=TestAlphaVantage._API_KEY_TEST)
+        try:
+            data, meta_data = av.get_daily('nonsense')
+            self.fail(msg='An error should be raised when using an non \
+            existent hey')
+        except ValueError:
+            self.assertTrue(True)
+
+    def test_get_daily_integrity(self):
+        """ Test that the data returned by the api is more or less
+        what we expected
+        """
+        av = AlphaVantage(key=TestAlphaVantage._API_KEY_TEST)
+        data, meta_data = av.get_daily(TestAlphaVantage._API_EQ_NAME_TEST)
+        self.assertTrue(len(data) == 100, "The dictionary size for the default \
+        outputsize ('compact') has to be 100")
+        data, meta_data = av.get_daily(TestAlphaVantage._API_EQ_NAME_TEST,
         outputsize='full')
         self.assertTrue(len(data) > 100, 'The dictionary size for the default \
         outputsize has to be 100')
