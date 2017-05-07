5878e09de845be47a3bf4f57e315ca0077e930ce
RomelTorres <torres.romel@gmail.com>
2017-05-07 15:12:49 +0200

    feat: Implement balance of power api call

--- alpha_vantage/alphavantage.py
+++ alpha_vantage/alphavantage.py
@@ -769,6 +769,22 @@ class AlphaVantage:
         _FUNCTION_KEY, symbol, interval, time_period, series_type, self.key)
         return self._handle_api_call(url,'Technical Analysis: MOM','Meta Data')
 
+    def get_bop(self, symbol, interval='60min', time_period=20):
+        """ Return the balance of power in two json
+        objects as data and meta_data. It raises ValueError when problems arise
+
+        Keyword arguments:
+        symbol -- the symbol for the equity we want to get its data
+        interval -- time interval between two conscutive values,
+        supported values are '1min', '5min', '15min', '30min', '60min', 'daily',
+        'weekly', 'monthly' (default '60min')
+        """
+        _FUNCTION_KEY = "BOP"
+        url = "{}function={}&symbol={}&interval={}&time_period={}"\
+        "&apikey={}".format(AlphaVantage._ALPHA_VANTAGE_API_URL,
+        _FUNCTION_KEY, symbol, interval, time_period, self.key)
+        return self._handle_api_call(url,'Technical Analysis: BOP','Meta Data')
+
 if __name__ == '__main__':
     av = AlphaVantage(key='486U')
     data, meta_data = av.get_sma('GOOGL')
