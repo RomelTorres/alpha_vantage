a97ae1cb450310bdb0402b00f1a283dbfa6de6fe
RomelTorres <torres.romel@gmail.com>
2017-05-08 22:00:46 +0200

    feat: Implement get true range api call

--- alpha_vantage/alphavantage.py
+++ alpha_vantage/alphavantage.py
@@ -1156,6 +1156,22 @@ class AlphaVantage:
         url = "{}&apikey={}".format(url, self.key)
         return self._handle_api_call(url,'Technical Analysis: SAR','Meta Data')
 
+    def get_trange(self, symbol, interval='60min'):
+        """ Return the true range values in two json
+        objects as data and meta_data. It raises ValueError when problems arise
+
+        Keyword arguments:
+        symbol -- the symbol for the equity we want to get its data
+        interval -- time interval between two conscutive values,
+        supported values are '1min', '5min', '15min', '30min', '60min', 'daily',
+        'weekly', 'monthly' (default '60min')
+        """
+        _FUNCTION_KEY = "TRANGE"
+        url = "{}function={}&symbol={}&interval={}&apikey={}".format(
+        AlphaVantage._ALPHA_VANTAGE_API_URL,_FUNCTION_KEY, symbol, interval,
+        self.key)
+        return self._handle_api_call(url,'Technical Analysis: TRANGE','Meta Data')
+
 if __name__ == '__main__':
     av = AlphaVantage(key='486U')
     data, meta_data = av.get_sma('GOOGL')
