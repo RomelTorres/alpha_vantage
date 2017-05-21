a3f9ca56d52b68289bffa522a72572e5e1b9837d
RomelTorres <torres.romel@gmail.com>
2017-05-21 11:36:41 +0200

    chores: Publishing on pipy

--- setup.py
+++ setup.py
@@ -3,17 +3,19 @@ from distutils.core import setup
 setup(
     name='alpha_vantage',
     version='0.1.2',
-    author='R. J. Torres',
-    author_email='rtorres@gmail.com',
+    author='Romel J. Torres',
+    author_email='romel.torres@gmail.com',
     license='MIT',
     description='Python module to get stock data from the Alpha Vantage Api',
-    long_description=open('README.md').read(),
+    url = 'https://github.com/RomelTorres/alpha_vantage',
+    download_url = 'https://github.com/RomelTorres/alpha_vantage/releases/tag/0.1.2/alpha_vantage-0.1.2.tar.gz',
     install_requires=[
         'simplejson',
         'pandas',
         'nose'
     ],
-    keywords='stocks market finance alpha_vantage quotes shares currency',
+    keywords=['stocks', 'market', 'finance', 'alpha_vantage', 'quotes',
+    'shares'],
     package_data={
         'alpha_vantage':[],
     }
