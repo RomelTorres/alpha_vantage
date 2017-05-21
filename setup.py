8a4791c90ff9010eb59b897a8805b5de7f6c56c4
RomelTorres <torres.romel@gmail.com>
2017-05-21 21:02:26 +0200

    fix: Eliminate download url from setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from distutils.core import setup
 
 setup(
     name='alpha_vantage',
-    version='0.1.3',
+    version='0.1.4',
     author='Romel J. Torres',
     author_email='romel.torres@gmail.com',
     license='MIT',
     description='Python module to get stock data from the Alpha Vantage Api',
     url = 'https://github.com/RomelTorres/alpha_vantage',
-    download_url = 'https://github.com/RomelTorres/alpha_vantage/releases/tag/0.1.3/alpha_vantage-0.1.3.tar.gz',
     install_requires=[
         'simplejson',
         'pandas',
