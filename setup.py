8b3ea61547a8851ae789fc30f49a971a8ae3994d
RomelTorres <torres.romel@gmail.com>
2017-05-23 20:12:55 +0200

    chore: Change wront topic in setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Finance',
+        'Topic :: Office/Business :: Financial :: Investment',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
