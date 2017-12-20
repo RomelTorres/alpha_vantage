4e5fe445884cf59768851ad8da10d64ac3ec5f61
Romel Torres <torres.romel@gmail.com>
2017-12-20 20:15:17 +0100

    Mock up techindicator call

--- test_alpha_vantage/test_alphavantage.py
+++ test_alpha_vantage/test_alphavantage.py
@@ -1,4 +1,9 @@
 from ..alpha_vantage.alphavantage import AlphaVantage
+from ..alpha_vantage.timeseries import TimeSeries
+from ..alpha_vantage.techindicators import TechIndicators
+from ..alpha_vantage.sectorperformance import SectorPerformances
+from ..alpha_vantage.cryptocurrencies import CryptoCurrencies
+from ..alpha_vantage.foreignexchange import ForeignExchange
 from pandas import DataFrame as df
 import unittest
 import mock
@@ -65,5 +70,65 @@ class TestAlphaVantage(unittest.TestCase):
         with open(path_file) as f:
             mock_urlopen.return_value = f
             data = av._handle_api_call(url)
+            self.assertIsInstance(
+                data, dict, 'Result Data must be a dictionary')
+
+    @unittest.skipIf(sys.version_info.major == 2, "Test valid for python 3")
+    @mock.patch('urllib.request.urlopen')
+    def test_time_series_intraday_python3(self, mock_urlopen):
+        """ Test that api call returns a json file as requested
+        """
+        ts = TimeSeries(key=TestAlphaVantage._API_KEY_TEST)
+        url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey=test"
+        path_file = self.get_file_from_url(url)
+        with open(path_file) as f:
+            mock_urlopen.return_value = f
+            data, _ = ts.get_intraday(
+                "MSFT", interval='1min', outputsize='full')
+            self.assertIsInstance(
+                data, dict, 'Result Data must be a dictionary')
+
+    @unittest.skipIf(sys.version_info.major == 3, "Test valid for python 3")
+    @mock.patch('urllib2.urlopen')
+    def test_time_series_intraday_python2(self, mock_urlopen):
+        """ Test that api call returns a json file as requested
+        """
+        ts = TimeSeries(key=TestAlphaVantage._API_KEY_TEST)
+        url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey=test"
+        path_file = self.get_file_from_url(url)
+        with open(path_file) as f:
+            mock_urlopen.return_value = f
+            data, _ = ts.get_intraday(
+                "MSFT", interval='1min', outputsize='full')
+            self.assertIsInstance(
+                data, dict, 'Result Data must be a dictionary')
+
+    @unittest.skipIf(sys.version_info.major == 2, "Test valid for python 3")
+    @mock.patch('urllib.request.urlopen')
+    def test_technical_indicator_sma_python3(self, mock_urlopen):
+        """ Test that api call returns a json file as requested
+        """
+        ti = TechIndicators(key=TestAlphaVantage._API_KEY_TEST)
+        url = "https://www.alphavantage.co/query?function=SMA&symbol=MSFT&interval=15min&time_period=10&series_type=close&apikey=test"
+        path_file = self.get_file_from_url(url)
+        with open(path_file) as f:
+            mock_urlopen.return_value = f
+            data, _ = ti.get_sma("MSFT", interval='15min',
+                                 time_period=10, series_type='close')
+            self.assertIsInstance(
+                data, dict, 'Result Data must be a dictionary')
+
+    @unittest.skipIf(sys.version_info.major == 3, "Test valid for python 3")
+    @mock.patch('urllib2.urlopen')
+    def test_technical_indicator_sma_python2(self, mock_urlopen):
+        """ Test that api call returns a json file as requested
+        """
+        ti = TechIndicators(key=TestAlphaVantage._API_KEY_TEST)
+        url = "https://www.alphavantage.co/query?function=SMA&symbol=MSFT&interval=15min&time_period=10&series_type=close&apikey=test"
+        path_file = self.get_file_from_url(url)
+        with open(path_file) as f:
+            mock_urlopen.return_value = f
+            data, _ = ti.get_sma("MSFT", interval='15min',
+                                 time_period=10, series_type='close')
             self.assertIsInstance(
                 data, dict, 'Result Data must be a dictionary')
