fbd3b77e053c5627e9c41b35f66fd74e0bbae9a5
Romel Torres <torres.romel@gmail.com>
2018-02-11 13:52:09 +0100

    Rework test

--- test_alpha_vantage/test_alphavantage.py
+++ test_alpha_vantage/test_alphavantage.py
@@ -56,7 +56,7 @@ class TestAlphaVantage(unittest.TestCase):
 
 
     @requests_mock.Mocker()
-    def test_time_series_intraday_python(self, mock_request):
+    def test_time_series_intraday(self, mock_request):
         """ Test that api call returns a json file as requested
         """
         ts = TimeSeries(key=TestAlphaVantage._API_KEY_TEST)
@@ -69,36 +69,20 @@ class TestAlphaVantage(unittest.TestCase):
             self.assertIsInstance(
                 data, dict, 'Result Data must be a dictionary')
 
-    # @unittest.skipIf(sys.version_info.major == 3, "Test valid for python  2.7")
-    # @mock.patch('urllib.urlopen')
-    # def test_time_series_intraday_python2(self, mock_urlopen):
-    #     """ Test that api call returns a json file as requested
-    #     """
-    #     ts = TimeSeries(key=TestAlphaVantage._API_KEY_TEST)
-    #     url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey=test"
-    #     path_file = self.get_file_from_url(url)
-    #     with open(path_file) as f:
-    #         mock_urlopen.return_value = f
-    #         data, _ = ts.get_intraday(
-    #             "MSFT", interval='1min', outputsize='full')
-    #         self.assertIsInstance(
-    #             data, dict, 'Result Data must be a dictionary')
-
-    # @unittest.skipIf(sys.version_info.major == 2, "Test valid for python 3")
-    # @mock.patch('urllib.request.urlopen')
-    # def test_time_series_intraday_pandas_python3(self, mock_urlopen):
-    #     """ Test that api call returns a json file as requested
-    #     """
-    #     ts = TimeSeries(key=TestAlphaVantage._API_KEY_TEST,
-    #                     output_format='pandas')
-    #     url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey=test"
-    #     path_file = self.get_file_from_url(url)
-    #     with open(path_file) as f:
-    #         mock_urlopen.return_value = f
-    #         data, _ = ts.get_intraday(
-    #             "MSFT", interval='1min', outputsize='full')
-    #         self.assertIsInstance(
-    #             data, df, 'Result Data must be a pandas data frame')
+    @requests_mock.Mocker()
+    def test_time_series_intraday_pandas(self, mock_request):
+        """ Test that api call returns a json file as requested
+        """
+        ts = TimeSeries(key=TestAlphaVantage._API_KEY_TEST,
+                        output_format='pandas')
+        url = "http://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&outputsize=full&apikey=test&datatype=json"
+        path_file = self.get_file_from_url("mock_time_series")
+        with open(path_file) as f:
+            mock_request.get(url, text=f.read())
+            data, _ = ts.get_intraday(
+                "MSFT", interval='1min', outputsize='full')
+            self.assertIsInstance(
+                data, df, 'Result Data must be a pandas data frame')
 
     # @unittest.skipIf(sys.version_info.major == 3, "Test valid for python 2")
     # @mock.patch('urllib.urlopen')
