1842f422e121d5eac94018fb457fee3db4b6fd6d
Patrick Collins <patrick@Patricks-MacBook-Pro-2.local>
2019-08-22 17:24:41 -0400

    Removed crypto intraday reference, added daily and exchange rate funtionality

--- alpha_vantage/cryptocurrencies.py
+++ alpha_vantage/cryptocurrencies.py
@@ -57,3 +57,20 @@ class CryptoCurrencies(av):
         """
         _FUNCTION_KEY = 'DIGITAL_CURRENCY_MONTHLY'
         return _FUNCTION_KEY, 'Time Series (Digital Currency Monthly)', 'Meta Data'
+
+    @av._output_format
+    @av._call_api_on_func
+    def get_digital_currency_exchange_rate(self, symbol, market):
+        """ Returns the current exchange rate for a digital currency
+        (e.g., BTC) traded on a specific market (e.g., CNY/Chinese Yuan),
+        and when it was last updated.
+
+        Keyword Arguments:
+            symbol: The digital/crypto currency of your choice. It can be any
+            of the currencies in the digital currency list. For example:
+            symbol=BTC.
+            market: The exchange market of your choice. It can be any of the
+            market in the market list. For example: market=CNY.
+        """
+        _FUNCTION_KEY = 'CURRENCY_EXCHANGE_RATE'
+        return _FUNCTION_KEY, 'Dictonary (Digital Currency Exchange Rate)', 'Meta Data'
