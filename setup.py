8817bebed99fd49f66a1fb2db21e8c57ed60b11c
PatrickAlphaVantage <patrick.collins@alphavantage.co>
2019-10-31 17:16:56 -0400

    Added VWAP

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except IOError:
 
 setup(
     name='alpha_vantage',
-    version='2.1.0',
+    version='2.1.1',
     author='Romel J. Torres',
     author_email='romel.torres@gmail.com',
     license='MIT',
