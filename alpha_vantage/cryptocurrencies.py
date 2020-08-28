993166630c9e69af6d58850e43312b95c973be4d
Ramon Carrasco <ramoncd@Ramons-MBP.attlocal.net>
2020-08-28 19:03:55 -0400

    adding crypto-rating functionality

--- alpha_vantage/cryptocurrencies.py
+++ alpha_vantage/cryptocurrencies.py
@@ -60,17 +60,14 @@ class CryptoCurrencies(av):
 
     @av._output_format
     @av._call_api_on_func
-    def get_digital_currency_exchange_rate(self, symbol, market):
-        """ Returns the current exchange rate for a digital currency
-        (e.g., BTC) traded on a specific market (e.g., CNY/Chinese Yuan),
-        and when it was last updated.
+    def get_digital_crypto_rating(self, symbol):
+        """ Returns the Fundamental Crypto Asset Score for a digital currency
+        (e.g., BTC), and when it was last updated.
 
         Keyword Arguments:
             symbol: The digital/crypto currency of your choice. It can be any
             of the currencies in the digital currency list. For example:
             symbol=BTC.
-            market: The exchange market of your choice. It can be any of the
-            market in the market list. For example: market=CNY.
         """
-        _FUNCTION_KEY = 'CURRENCY_EXCHANGE_RATE'
-        return _FUNCTION_KEY, 'Dictonary (Digital Currency Exchange Rate)', 'Meta Data'
+        _FUNCTION_KEY = 'CRYPTO_RATING'
+        return _FUNCTION_KEY, 'Crypto Rating (FCAS)', None
